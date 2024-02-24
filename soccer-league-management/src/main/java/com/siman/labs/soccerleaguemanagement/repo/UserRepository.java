6661a7af62fe7bb49b51683c1c33901f8e60bb99
Team Siman Labs <160309844+teamsimanlabs@users.noreply.github.com>
2024-02-24 13:01:45 -0600

    B initial project (#3)
    * modified readme
    
    * added starter code
    
    * added to data.sql
    
    * removed unwanted comments
    
    ---------
    
    Co-authored-by: shire farah <99207326+shireaa@users.noreply.github.com>

--- soccer-league-management/src/main/java/com/siman/labs/soccerleaguemanagement/repo/UserRepository.java
+++ soccer-league-management/src/main/java/com/siman/labs/soccerleaguemanagement/repo/UserRepository.java
@@ -1,6 +1,6 @@
 package com.siman.labs.soccerleaguemanagement.repo;
 
-import com.siman.labs.soccerleaguemanagement.model.User;
+import com.siman.labs.soccerleaguemanagement.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
---
+++ soccer-league-management/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into _user (first_name, last_name, password, roles, user_name) values ('baba', 'bobo', '$2a$10$mPiSV0sLOMzHLek0B9sV4OyeNdZa6EHxNQc8nvzAbr6xoI/NEAreS', 'ADMIN', 'abdikarim');
+insert into _user (first_name, last_name, password, roles, user_name) values ('mangala', 'big_head', '$2a$10$mPiSV0sLOMzHLek0B9sV4OyeNdZa6EHxNQc8nvzAbr6xoI/NEAreS', 'ADMIN', 'mangala');
+insert into _user (first_name, last_name, password, roles, user_name) values ('musa', 'sy', '$2a$10$mPiSV0sLOMzHLek0B9sV4OyeNdZa6EHxNQc8nvzAbr6xoI/NEAreS', 'ADMIN', 'musa');
+insert into _user (first_name, last_name, password, roles, user_name) values ('shire', 'farah', '$2a$10$mPiSV0sLOMzHLek0B9sV4OyeNdZa6EHxNQc8nvzAbr6xoI/NEAreS', 'ADMIN', 'shire');
+insert into _user (first_name, last_name, password, roles, user_name) values ('shire', 'farah', '$2a$10$mPiSV0sLOMzHLek0B9sV4OyeNdZa6EHxNQc8nvzAbr6xoI/NEAreS', 'ADMIN', 'shuram');
+insert into _user (first_name, last_name, password, roles, user_name) values ('shire', 'farah', '$2a$10$mPiSV0sLOMzHLek0B9sV4OyeNdZa6EHxNQc8nvzAbr6xoI/NEAreS', 'ADMIN', 'siman');
+insert into _user (first_name, last_name, password, roles, user_name) values ('shire', 'farah', '$2a$10$mPiSV0sLOMzHLek0B9sV4OyeNdZa6EHxNQc8nvzAbr6xoI/NEAreS', 'ADMIN', 'dieynaba');
