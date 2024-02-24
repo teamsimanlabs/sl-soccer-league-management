bb7edfb026569389af51eb2dfaca81c44fd931c7
shire farah <99207326+shireaa@users.noreply.github.com>
2024-02-24 13:01:08 -0600

    removed unwanted comments

--- soccer-league-management/src/main/resources/data.sql
+++ soccer-league-management/src/main/resources/data.sql
@@ -5,10 +5,3 @@ insert into _user (first_name, last_name, password, roles, user_name) values ('s
 insert into _user (first_name, last_name, password, roles, user_name) values ('shire', 'farah', '$2a$10$mPiSV0sLOMzHLek0B9sV4OyeNdZa6EHxNQc8nvzAbr6xoI/NEAreS', 'ADMIN', 'shuram');
 insert into _user (first_name, last_name, password, roles, user_name) values ('shire', 'farah', '$2a$10$mPiSV0sLOMzHLek0B9sV4OyeNdZa6EHxNQc8nvzAbr6xoI/NEAreS', 'ADMIN', 'siman');
 insert into _user (first_name, last_name, password, roles, user_name) values ('shire', 'farah', '$2a$10$mPiSV0sLOMzHLek0B9sV4OyeNdZa6EHxNQc8nvzAbr6xoI/NEAreS', 'ADMIN', 'dieynaba');
---
---
--- first_name varchar(255),
---         last_name varchar(255),
---         password varchar(255),
---         role varchar(255) check (role in ('USER','ADMIN')),
---         user_name varchar(255),
